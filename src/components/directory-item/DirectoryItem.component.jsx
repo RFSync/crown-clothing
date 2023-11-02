2a877f9ea58766cdda59b412bc1c471e3fbc3c7e
Rafael Fernandez <myztajay@hotmail.com>
2023-11-02 16:31:28 -0400

    Added linking from category page

--- src/components/directory-item/DirectoryItem.component.jsx
+++ src/components/directory-item/DirectoryItem.component.jsx
@@ -1,10 +1,18 @@
 import { Fragment } from "react";
 import "./directory-item.styles.scss";
+import { useNavigate } from "react-router";
 
 const DirectoryItem = ({ category }) => {
-	const { title, id, imageUrl } = category;
+	const { title, imageUrl } = category;
+	const navigate = useNavigate();
+	const handleGoToCategory = (title) => {
+		navigate(`shop/${title}`);
+	};
 	return (
-		<div className='directory-item-container'>
+		<div
+			className='directory-item-container'
+			onClick={() => handleGoToCategory(title)}
+		>
 			<div
 				className='background-image'
 				style={{
--- src/components/directory/Directory.component.jsx
+++ src/components/directory/Directory.component.jsx
@@ -1,11 +1,23 @@
 import DirectoryItem from "../directory-item/DirectoryItem.component";
 import "./directory.styles.scss";
+import { useNavigate } from "react-router";
 
 const Directory = ({ categories }) => {
+	const navigate = useNavigate();
+	const handleGoToCategory = (title) => {
+		debugger;
+		navigate(`/${title}`);
+	};
 	return (
 		<div className='directory-container'>
 			{categories.map((category) => {
-				return <DirectoryItem key={category.id} category={category} />;
+				return (
+					<DirectoryItem
+						key={category.id}
+						onClick={console.log("ccc")}
+						category={category}
+					/>
+				);
 			})}
 		</div>
 	);
