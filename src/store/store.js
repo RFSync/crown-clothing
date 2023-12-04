1d5231c3b013f5dd15b5384ac4b7c0a13c5474b2
Rafael Fernandez <myztajay@hotmail.com>
2023-12-04 16:34:06 -0500

    Added persist library for state prusisten on local storage

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,24 @@ import { compose, legacy_createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 import { Logger } from "sass";
+import { persistStore } from "redux-persist";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from "redux-persist/lib/storage";
+import { userReducer } from "./user/user.reducer";
 
 const middleWares = [logger];
 const composeEnhancers = compose(applyMiddleware(...middleWares));
+
+const persistConfig = {
+	key: "root",
+	storage,
+	blacklist: "user",
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 //rootreducer
 export const store = legacy_createStore(
-	rootReducer,
+	persistedReducer,
 	undefined,
 	composeEnhancers
 );
+export const persistor = persistStore(store);
